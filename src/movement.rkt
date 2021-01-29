242e3a443c3f26569ec9e5f583820ae413bf5efd
Brian Adkins <brian@lojic.com>
2021-01-29 18:08:24 -0500

    Re-order functions

--- src/movement.rkt
+++ src/movement.rkt
@@ -14,15 +14,27 @@
          generate-moves!
          print-move)
 
-(define (add-quiet-move! b m)
-  (let ([ head (add1 (quiet-head b)) ])
-    (set-quiet-head! b head)
-    (vector-set! (quiet-moves b) head m)))
+;; --------------------------------------------------------------------------------------------
+;; Public Interface
+;; --------------------------------------------------------------------------------------------
 
-(define (add-tactical-move! b m)
-  (let ([ head (add1 (tactical-head b)) ])
-    (set-tactical-head! b head)
-    (vector-set! (tactical-moves b) head m)))
+(define (generate-moves! b)
+  (define is-white? (board-whites-move? b))
+
+  (init-moves! b)
+
+  (for* ([ rank (in-range 8) ]
+         [ file (in-range 8) ])
+    (let* ([ idx   (+ 21 file (* rank 10))           ]
+           [ piece (bytes-ref (board-squares b) idx) ])
+      (when (is-right-color-piece? piece is-white?)
+        (match (piece-type piece)
+          [ #b001 (generate-pawn-moves! b idx piece)   ]
+          [ #b010 (generate-knight-moves! b idx piece) ]
+          [ #b011 (generate-bishop-moves! b idx piece) ]
+          [ #b100 (generate-rook-moves! b idx piece)   ]
+          [ #b101 (generate-queen-moves! b idx piece)  ]
+          [ #b110 (generate-king-moves! b idx piece)   ])))))
 
 (define (generate-bishop-moves! b idx piece)
   (generate-sliding-moves! b idx piece north-east)
@@ -33,6 +45,46 @@
 (define (generate-king-moves! b idx piece)
   (generate-offset-moves! b idx piece king-offsets)
   (generate-castle-moves! b idx piece))
+
+(define (generate-knight-moves! b idx piece)
+  (generate-offset-moves! b idx piece knight-offsets))
+
+(define (generate-queen-moves! b idx piece)
+  (generate-bishop-moves! b idx piece)
+  (generate-rook-moves! b idx piece))
+
+(define (generate-rook-moves! b idx piece)
+  (generate-sliding-moves! b idx piece north)
+  (generate-sliding-moves! b idx piece east)
+  (generate-sliding-moves! b idx piece south)
+  (generate-sliding-moves! b idx piece west))
+
+(define (print-move m)
+  (printf "~a~a~a~a"
+          (piece-symbol (move-src m))
+          (idx->pos (move-src-idx m))
+          (if (move-captured-piece m)
+              "x"
+              "-")
+          (idx->pos (move-dst-idx m)))
+  (let ([ promoted (move-promoted-piece m) ])
+    (when promoted
+      (printf "=~a" (piece-symbol promoted))))
+  (printf "\n"))
+
+;; --------------------------------------------------------------------------------------------
+;; Private Implementation
+;; --------------------------------------------------------------------------------------------
+
+(define (add-quiet-move! b m)
+  (let ([ head (add1 (quiet-head b)) ])
+    (set-quiet-head! b head)
+    (vector-set! (quiet-moves b) head m)))
+
+(define (add-tactical-move! b m)
+  (let ([ head (add1 (tactical-head b)) ])
+    (set-tactical-head! b head)
+    (vector-set! (tactical-moves b) head m)))
 
 (define (generate-castle-moves! b idx piece)
   (when (not (has-moved? piece)) ; King has not moved
@@ -54,27 +106,6 @@
                    (= empty-square (bytes-ref squares (- idx 3))))
           (add-quiet-move! b
                            (create-move piece idx (- idx 2) #:is-castle-queenside? #t)))))))
-
-(define (generate-knight-moves! b idx piece)
-  (generate-offset-moves! b idx piece knight-offsets))
-
-(define (generate-moves! b)
-  (define is-white? (board-whites-move? b))
-
-  (init-moves! b)
-
-  (for* ([ rank (in-range 8) ]
-         [ file (in-range 8) ])
-    (let* ([ idx   (+ 21 file (* rank 10))           ]
-           [ piece (bytes-ref (board-squares b) idx) ])
-      (when (is-right-color-piece? piece is-white?)
-        (match (piece-type piece)
-          [ #b001 (generate-pawn-moves! b idx piece)   ]
-          [ #b010 (generate-knight-moves! b idx piece) ]
-          [ #b011 (generate-bishop-moves! b idx piece) ]
-          [ #b100 (generate-rook-moves! b idx piece)   ]
-          [ #b101 (generate-queen-moves! b idx piece)  ]
-          [ #b110 (generate-king-moves! b idx piece)   ])))))
 
 (define (generate-offset-moves! b idx piece offsets)
   (let ([ squares (board-squares b) ])
@@ -218,29 +249,6 @@
               ;; Add tactical move for capture and exit
               (add-tactical-move! b
                                  (create-move piece idx target-idx #:captured-piece target)) ]))))
-
-(define (generate-queen-moves! b idx piece)
-  (generate-bishop-moves! b idx piece)
-  (generate-rook-moves! b idx piece))
-
-(define (generate-rook-moves! b idx piece)
-  (generate-sliding-moves! b idx piece north)
-  (generate-sliding-moves! b idx piece east)
-  (generate-sliding-moves! b idx piece south)
-  (generate-sliding-moves! b idx piece west))
-
-(define (print-move m)
-  (printf "~a~a~a~a"
-          (piece-symbol (move-src m))
-          (idx->pos (move-src-idx m))
-          (if (move-captured-piece m)
-              "x"
-              "-")
-          (idx->pos (move-dst-idx m)))
-  (let ([ promoted (move-promoted-piece m) ])
-    (when promoted
-      (printf "=~a" (piece-symbol promoted))))
-  (printf "\n"))
 
 (define (print-moves b)
   ;; Tactical moves
