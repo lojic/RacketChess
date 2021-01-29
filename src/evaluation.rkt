62269c474e5fdd234d14cfad9ca02209f73fe289
Brian Adkins <brian@lojic.com>
2021-01-28 20:57:39 -0500

    add simple heuristics to evaluate

--- src/evaluation.rkt
+++ src/evaluation.rkt
@@ -3,6 +3,8 @@
 (require "./board.rkt"
          "./piece.rkt")
 
+(require racket/performance-hint)
+
 (provide evaluate)
 
 ;; For now, simply sum the piece values, and give slight preference
@@ -10,18 +12,46 @@
 (define (evaluate b)
   (for*/sum ([ rank (in-range 8) ]
              [ file (in-range 8) ])
-    (let* ([ idx   (file-rank->idx file rank)        ]
-           [ piece (bytes-ref (board-squares b) idx) ])
-      (cond [ (is-pawn? piece)  (evaluate-pawn b piece idx) ]
-            [ (is-piece? piece) (piece-value piece)         ]
-            [ else              0.0                         ]))))
+    (let* ([ idx    (file-rank->idx file rank)        ]
+           [ piece  (bytes-ref (board-squares b) idx) ])
+      (if (= piece empty-square)
+          0.0
+          (let ([ white? (is-white? piece)   ]
+                [ val    (piece-value piece) ])
+            (cond [ (is-pawn? piece)   (evaluate-pawn white? val idx)     ]
+                  [ (is-king? piece)   (evaluate-king white? val idx)     ]
+                  [ (is-queen? piece)  val                                ]
+                  [ (is-rook? piece)   val                                ]
+                  [ (is-bishop? piece) (evaluate-bishop piece white? val) ]
+                  [ (is-knight? piece) (evaluate-knight piece white? val) ]))))))
 
-;; Not sure if this is a good idea, but give the 4 center positions
-;; higher value to encourage controlling the center.
-(define (evaluate-pawn b piece idx)
-  (let ([ val (piece-value piece) ])
-    (cond [ (= idx 64) (* val 1.02) ]
-          [ (= idx 65) (* val 1.02) ]
-          [ (= idx 54) (* val 1.02) ]
-          [ (= idx 55) (* val 1.02) ]
-          [ else       val          ])))
+(define-inline (evaluate-bishop piece white? val)
+  ;; Slightly higher value for deployed bishop
+  (if (has-moved? piece)
+      ((if white? + -) val 0.01)
+      val))
+
+(define-inline (evaluate-king white? val idx)
+  ;; Slightly higher value for king on castled square
+  (if white?
+      (if (or (= idx 93) (= idx 97))
+          (+ val 0.05)
+          val)
+      (if (or (= idx 23) (= idx 27))
+          (- val 0.05)
+          val)))
+
+(define-inline (evaluate-knight piece white? val)
+  ;; Slightly higher value for deployed knight
+  (if (has-moved? piece)
+      ((if white? + -) val 0.01)
+      val))
+
+(define-inline (evaluate-pawn white? val idx)
+  (if white?
+      (if (or (= idx 64) (= idx 65))
+          (+ val 0.05)
+          val)
+      (if (or (= idx 54) (= idx 55))
+          (- val 0.05)
+          val)))
