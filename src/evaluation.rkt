5431ce2f67e97988e0ea44eafdcca1e7515d91ae
Brian Adkins <brian@lojic.com>
2021-02-03 16:29:18 -0500

    Allow evaluating a FEN position

--- src/evaluation.rkt
+++ src/evaluation.rkt
@@ -59,3 +59,10 @@
       (if (or (= idx 54) (= idx 55))
           (- val 5)
           val)))
+
+(module+ main
+  (require "./fen.rkt")
+  ;; Queen takes pawn, bishop takes pawn:  r4rk1/ppp2ppp/2n5/3Bp3/8/2P4q/P1PPKP2/R1BQ4 b - - 0 1
+  ;; Queen back to e4:  r4rk1/ppp2ppp/2n5/3pp3/4q3/1BP4P/P1PP1P2/R1BQ1K2 b - - 0 1
+  (let ([ b (fen->board "r4rk1/ppp2ppp/2n5/3Bp3/8/2P4q/P1PPKP2/R1BQ4 b - - 0 1") ])
+    (printf "Evaluation = ~a\n" (evaluate b))))
