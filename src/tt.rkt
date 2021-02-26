c9b08de78ba77a1148ae433fd3b58d71a1636b14
Brian Adkins <brian@lojic.com>
2021-02-26 15:55:11 -0500

    adjust mate scores by ply

--- src/tt.rkt
+++ src/tt.rkt
@@ -1,10 +1,13 @@
- #lang racket
+#lang racket
 
 (require "./global.rkt")
 (require racket/performance-hint)
 
 (provide read-tt-entry
          write-tt-entry!
+         MATE-SCORE
+         MAX-SCORE
+         MIN-SCORE
          NODE-ALPHA
          NODE-BETA
          NODE-EXACT)
@@ -13,6 +16,11 @@
 ;; simply do an 'and' to obtain the index
 (define NUM-HASH-ENTRIES (expt 2 28))
 (define HASH-MASK (sub1 NUM-HASH-ENTRIES))
+
+(define MIN-SCORE -100000)
+(define MAX-SCORE  100000)
+(define MATE-SCORE  90000)
+(define MATE-VALUE  80000)
 
 (define NODE-EXACT 1)
 (define NODE-BETA  2)
@@ -31,14 +39,19 @@
   (fxand zobrist-key HASH-MASK))
 
 ;; Returns (values score move). If no matching entry found, both score and move are #f
-(define (read-tt-entry key draft alpha beta)
+(define (read-tt-entry key draft ply alpha beta)
+  (define-inline (adjust-score score ply)
+    (cond [ (fx< score (fx- MATE-VALUE)) (fx+ score ply) ]
+          [ (fx> score MATE-VALUE)       (fx- score ply) ]
+          [ else                         score           ]))
+
   (let ([ entry (vecref tt-table (hash-index key)) ])
     (if (and entry (fx= key (tt-zobrist-key entry)))
         ;; Entry found, and it matches the key
         (let ([ best-move (tt-best-move entry) ])
           (if (fx>= (tt-draft entry) draft)
               ;; We have sufficient draft
-              (let ([ score (tt-score entry) ]
+              (let ([ score (adjust-score (tt-score entry) ply) ]
                     [ type  (tt-type entry)  ])
                 (cond [ (fx= NODE-EXACT type)                          (values score best-move) ]
                       [ (and (fx= NODE-ALPHA type) (fx<= score alpha)) (values alpha best-move) ]
@@ -50,14 +63,17 @@
         ;; Nothing found
         (values #f #f))))
 
-(define (write-tt-entry! key draft score best-move type)
-  (vecset! tt-table
-           (hash-index key)
-           (tt key
-               draft
-               best-move
-               type
-               score)))
+(define (write-tt-entry! key draft ply score best-move type)
+  (let ([ score (cond [ (fx< score (fx- MATE-SCORE)) (fx- score ply) ]
+                      [ (fx> score MATE-SCORE)       (fx+ score ply) ]
+                      [ else                         score           ]) ])
+    (vecset! tt-table
+             (hash-index key)
+             (tt key
+                 draft
+                 best-move
+                 type
+                 score))))
 
 (module+ test
   (require math/base
