213f83c14d2d1ed4f9fac3d6d22e5e8419b7932c
Brian Adkins <brian@lojic.com>
2021-02-26 15:55:42 -0500

    Big refactoring of search
    Factored out code into functions
    Introduced separate root alpha beta
    Plays well, but seems slower, so I may try unfolding the functions

--- src/search.rkt
+++ src/search.rkt
@@ -14,9 +14,6 @@
 
 (provide search)
 
-(define MIN-SCORE -100000)
-(define MAX-SCORE 100000)
-
 (define (search b max-level seconds)
   (define stats-obj (create-stats))
   (init-timer! stats-obj seconds)
@@ -29,13 +26,14 @@
           best)
         (begin
           (set-board-depth! b 0)
-          (let ([ result (alpha-beta! b level stats-obj MIN-SCORE MAX-SCORE) ])
+          (let* ([ best-move (if best (cdr best) #f) ]
+                 [ result (alpha-beta-root! b level stats-obj MIN-SCORE MAX-SCORE best-move) ])
             (if result
                 (let ([ score (car result) ]
                       [ m     (cdr result) ])
                   (if m
                       (begin
-                        (printf "Best move (~a): " level)
+                        (printf "Best move (~a) [~a] " level (~r (/ score 100) #:precision 2))
                         (print-move m)
                         (loop (fx+ 1 level) (cons score m)))
                       (begin
@@ -45,75 +43,113 @@
                   (print-stats stats-obj)
                   best)))))))
 
+(define-inline (search-move get-move main-loop stats-obj depth max-level b m alpha beta best-move type legal-move?)
+  (make-move! b m)
+  (cond [ (is-legal? b m)
+          ;; Legal move, continue
+          (let* ([ result (alpha-beta! b max-level stats-obj (fx- beta) (fx- alpha)) ]
+                 [ score  (and result (fx- result)) ])
+            (unmake-move! b m)
+            (cond [ (not score)
+                    #f ]
+                  [ (fx>= score beta)
+                    (increment-beta-cuts! stats-obj)
+                    (write-tt-entry! (get-hash-key) max-level depth beta m NODE-BETA)
+                    beta ]
+                  [ (fx> score alpha)
+                    (main-loop get-move depth score m NODE-EXACT #t) ]
+                  [ else
+                    (main-loop get-move depth alpha best-move type #t) ])) ]
+        [ else
+          ;; Illegal move, ignore move
+          (unmake-move! b m)
+          (main-loop get-move depth alpha best-move type legal-move?) ]))
+
+;; Returns (cons score move)
+(define (alpha-beta-root! b max-level stats-obj alpha beta prev-best-move)
+  ;; Helper functions -------------------------------------------------------------------------
+  (define (return-score-move alpha move type legal-move?)
+    (cond [ move
+            (write-tt-entry! (get-hash-key) max-level 0 alpha move type)
+            (cons alpha move) ]
+          [ legal-move?
+            (write-tt-entry! (get-hash-key) max-level 0 alpha #f type)
+            (cons alpha #f) ]
+          [ else
+            ;; No legal moves, so either stalemate or checkmate
+            (cond [ (is-king-in-check? b)
+                    ;; Checkmate
+                    (write-tt-entry! (get-hash-key) max-level 0 (fx- MATE-SCORE) #f NODE-EXACT)
+                    (cons MIN-SCORE #f) ]
+                  [ else
+                    ;; Stalemate
+                    (write-tt-entry! (get-hash-key) max-level 0 0 #f NODE-EXACT)
+                    (cons 0 #f) ]) ]))
+
+  (define (main-loop get-move _ alpha best-move type legal-move?)
+    (let ([ m (get-move) ])
+      (if m
+          (search-move get-move main-loop stats-obj 0 max-level b m alpha beta best-move type legal-move?)
+          (return-score-move alpha best-move type legal-move?))))
+  ;; ------------------------------------------------------------------------------------------
+
+  (increment-nodes! stats-obj)
+  (define get-move (move-iterator! b #:tt-move prev-best-move))
+  (main-loop get-move 0 alpha #f NODE-ALPHA #f))
+
 (define (alpha-beta! b max-level stats-obj alpha beta)
+  ;; Helper functions -------------------------------------------------------------------------
+  (define (return-score depth alpha move type legal-move?)
+    (cond [ move
+            (write-tt-entry! (get-hash-key) (fx- max-level depth) depth alpha move type)
+            alpha ]
+          [ legal-move?
+            (write-tt-entry! (get-hash-key) (fx- max-level depth) depth alpha #f type)
+            alpha ]
+          [ else
+            ;; No legal moves, so either stalemate or checkmate
+            (cond [ (is-king-in-check? b)
+                    (let ([ score (fx+ (fx- MATE-SCORE) depth) ])
+                      (write-tt-entry! (get-hash-key) (fx- max-level depth) depth
+                                       score #f NODE-EXACT)
+                      score) ]
+                  [ else
+                    (write-tt-entry! (get-hash-key) (fx- max-level depth) depth
+                                     0 #f NODE-EXACT)
+                    0 ]) ]))
+
+  (define (main-loop get-move depth alpha best-move type legal-move?)
+    (let ([ m (get-move) ])
+      (if m
+          (search-move get-move main-loop stats-obj depth max-level b m alpha beta best-move type legal-move?)
+          (return-score depth alpha best-move type legal-move?))))
+  ;; ------------------------------------------------------------------------------------------
+
   (increment-nodes! stats-obj)
   (define depth (board-depth b))
   (define-values (tt-score tt-move)
-    (if (fx> depth 0)
-        (read-tt-entry (get-hash-key) (fx- max-level depth) alpha beta)
-        (values #f #f)))
-
+    (read-tt-entry (get-hash-key) (fx- max-level depth) depth alpha beta))
   (when tt-move (increment-tt-moves! stats-obj))
 
-  (cond [ (and tt-score tt-move (fx< alpha tt-score beta))
+  (cond [ tt-score
           ;; Found a TT entry
           (increment-tt-hits! stats-obj)
-          (if (fx= depth 0)
-              (begin
-                (printf "Returning TT move: ")
-                (print-move tt-move)
-                (cons tt-score tt-move))
-              tt-score) ]
+          tt-score ]
         [ else
           ;; No TT entry found
           (increment-tt-misses! stats-obj)
-          (cond [ (timeout? stats-obj) #f ]
+          (cond [ (timeout? stats-obj)
+                  ;; Timeout - indicate no result
+                  #f ]
                 [ (fx= depth max-level)
-                  (quiesce! b stats-obj alpha beta) ]
+                  ;; Maximum depth - quiesce
+                  (let ([ val (quiesce! b stats-obj alpha beta) ])
+                    (write-tt-entry! (get-hash-key) 0 depth val #f NODE-EXACT)
+                    val) ]
                 [ else
+                  ;; Otherwise, run main loop
                   (let ([ get-move (move-iterator! b #:tt-move tt-move) ])
-                    (let loop ([ legal-move? #f         ]
-                               [ alpha       alpha      ]
-                               [ move        #f         ]
-                               [ type        NODE-ALPHA ])
-                      (let ([ m (get-move) ])
-                        (if (or (not m) (fx>= alpha beta))
-                            ;; No more moves, or alpha >= beta
-                            ;; If we're at the top level, return move &
-                            ;; score; otherwise, just score.
-                            (if (fx= depth 0)
-                                (cons alpha move)
-                                (cond [ move
-                                        (write-tt-entry! (get-hash-key) (fx- max-level depth) alpha move type)
-                                        alpha ]
-                                      [ legal-move?
-                                        alpha ]
-                                      [ else
-                                        ;; No legal moves, so either stalemate or checkmate
-                                        (if (is-king-in-check? b)
-                                            (fx+ MIN-SCORE depth)
-                                            0) ]))
-                            (begin
-                              (make-move! b m)
-                              (if (is-legal? b m)
-                                  ;; Legal move, continue
-                                  (let* ([ result (alpha-beta! b max-level stats-obj (fx- beta) (fx- alpha)) ]
-                                         [ score  (and result (fx- result)) ])
-                                    (unmake-move! b m)
-                                    (cond [ (not score)
-                                            #f ]
-                                          [ (fx>= score beta)
-                                            (increment-beta-cuts! stats-obj)
-                                            (write-tt-entry! (get-hash-key) (fx- max-level depth) beta move NODE-BETA)
-                                            beta ]
-                                          [ (fx> score alpha)
-                                            (loop #t score m NODE-EXACT) ]
-                                          [ else
-                                            (loop #t alpha move type) ]))
-                                  ;; Illegal move, ignore move
-                                  (begin
-                                    (unmake-move! b m)
-                                    (loop legal-move? alpha move type)))))))) ]) ]))
+                    (main-loop get-move depth alpha tt-move NODE-ALPHA #f)) ]) ]))
 
 (define (quiesce! b stats-obj alpha beta)
   (increment-quiesce-nodes! stats-obj)
